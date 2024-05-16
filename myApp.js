42f00fa57f4f50a7abf8ce9e5eaed0db31bebdfb
Jake Samuels <jakesamuels@Jakes-MacBook-Pro.local>
2024-05-16 13:06:28 +0100

    Serve static style.css file

--- myApp.js
+++ myApp.js
@@ -1,10 +1,11 @@
 let express = require("express");
 let app = express();
+let path = require("path");
 
-const absolutePath = __dirname + "/views/index.html";
+app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(absolutePath);
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 module.exports = app;
